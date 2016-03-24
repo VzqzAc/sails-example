d7a86f295f764390023eb7d652b50db69daad1c5
Omar V <omar.vazquezac@gmail.com>
2016-03-23 23:27:22 -0600

    created homepagecontroller

---
+++ api/controllers/HomepagesController.js
@@ -0,0 +1,15 @@
+/**
+ * HomepagesController
+ *
+ * @description :: Server-side logic for managing Homepages
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	index: function (request, response) {
+		return response.view('homepage', {
+			currentDate: (new Date()).toString()
+		});
+	}
+};
+
